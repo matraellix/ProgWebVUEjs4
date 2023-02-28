f81d7b78b3c04f6b545d08c4d352bc17ebf5dfe2
Maëlle Fleitz <maelle.fleitz@gmail.com>
2023-02-28 17:28:42 +0100

    premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="superhero-gallery">
+    <SuperHeroCard name="Batman"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SuperHeroCard from './components/SuperHeroCard.vue'
 
 export default {
-  name: 'App',
+  name: 'SuperHeroApp',
   components: {
-    HelloWorld
+    SuperHeroCard
   }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/SuperHeroCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="superhero-card">
+        <img class="image" src="https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg"/>
+        <div>
+        <h2 class="name">{{name}}</h2>
+        </div>  
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SuperHeroCard',
+        props: {
+            name: {type: String, required: true},
+            image: {type:String, default:"https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg"}
+        }
+    }
+</script>
+
+<style scoped>
+    .picture {
+        width:10em;
+        height:10em;
+    }
+    .superhero-card {
+        display: flex;
+    }
+
+    h2 {
+        font-size: 20px;
+    }
+</style>
